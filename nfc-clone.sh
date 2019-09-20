fc38d5118b830a077b11e502e0f09f7e32c25dcf
Shen Sijie <sjshen@pku.edu.cn>
2019-09-20 23:45:02 +0800

    Version 2

--- nfc-clone.sh
+++ nfc-clone.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+trap 'rm -f "$TMP_FILE"' EXIT
 
 function enter_to_continue() {
     read -p "Press [enter] to continue."
 }
 
-function clear_and_exit() {
-    rm -f temp.mfd
-    exit $1
+check_return_value() {
+    if [ $? -ne 0 ]
+    then
+        echo -e "\033[1;31mError:\033[0m $1."
+        exit -1
+    fi
 }
 
 clear
@@ -20,35 +24,28 @@ echo "Please connect your PN532 board to this computer and prepare an empty UID 
 enter_to_continue
 
 # Scan NFC device
-nfc-scan-device 2>&1 | grep error
-if [ $? == 0 ]
-then
-    echo -e "\033[1;31mError:\033[0m cannot connect to PN532 board."
-    clear_and_exit -1
-fi
+! nfc-scan-device 2>&1 | grep error
+check_return_value "cannot connect to PN532 board"
 
 # Read and crack NFC card
 clear
 echo "== Step 1: Put your original NFC card on the PN532 board."
 enter_to_continue
-mfoc -O temp.mfd
-if [ $? -ne 0 ]
-then
-    echo -e "\033[1;31mError:\033[0m cannot crack the card."
-    clear_and_exit -1
-fi
+
+TMP_FILE=`mktemp tmp.XXXXXXXXXXXXXXXX`
+check_return_value "cannot make temporary file"
+
+mfoc -O $TMP_FILE
+check_return_value "cannot crack the card"
 
 # Write empty card
 clear
 echo "== Step 2: Remove the original card and put your empty UID card on the PN532 board."
 enter_to_continue
-CARD_UID=`hexdump -n 4 -e  '4 1 "%02x"' temp.mfd`
+
+CARD_UID=`hexdump -n 4 -e  '4 1 "%02x"' $TMP_FILE`
 nfc-mfsetuid $CARD_UID
-if [ $? -ne 0 ]
-then
-    echo -e "\033[1;31mError:\033[0m cannot write UID of the empty card."
-    clear_and_exit -1
-fi
+check_return_value "cannot write UID of the empty card"
 
 # Write real card
 clear
@@ -56,11 +53,8 @@ echo "== Step 3: Put the target card on the PN532 board."
 echo "   * Clone to the empty card, just put the empty card on the board."
 echo "   * Clone to your phone or band, please open a new keycard, enable it, and put your phone/band on the board."
 enter_to_continue
-nfc-mfclassic w a temp.mfd
-if [ $? -ne 0 ]
-then
-    echo -e "\033[1;31mError:\033[0m cannot write the target card."
-    clear_and_exit -1
-fi
 
-clear_and_exit 0
+nfc-mfclassic w a $TMP_FILE
+check_return_value "cannot write the target card"
+
+exit 0
